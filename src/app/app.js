0b8e17b323cce0293fd82e9559302ef2e8688077
Lcisnerosm <53229664+Lcisnerosm@users.noreply.github.com>
2020-11-15 23:16:19 -0600

    commit

--- src/app/app.js
+++ src/app/app.js
@@ -7,10 +7,10 @@ const port = (process.env.PORT || 4002)
 const app = express()
 
 app.set('port', port)
+app.use(cors())
 
 app. use(express.urlencoded({extended: false}))
 app.use(express.json())
-app.use(cors())
 
 
 app.use('/api', Auth, require('./routes/route'))
